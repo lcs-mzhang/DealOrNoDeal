21fc208180f0ec85ffec4673425c027967e188f6
Zhang Matt <mzhang@lcs.on.ca>
2018-05-22 10:12:09 -0400

    I force unwrapped some stuff but it's done!

--- DealOrNoDeal/main.swift
+++ DealOrNoDeal/main.swift
@@ -7,7 +7,34 @@ import Foundation
 
 // INPUT
 // Global variable to use later in program
-var countOfBriefcasesOpened = 3
+var openedCases = 0
+var cases: [Int : Int] = [
+    1 : 100,
+    2 : 500,
+    3 : 1000,
+    4 : 5000,
+    5 : 10000,
+    6 : 25000,
+    7 : 50000,
+    8 : 100000,
+    9 : 500000,
+    10 : 1000000
+]
+
+while true {
+    print("How many briefcases have been opened?")
+    guard let givenInput = readLine() else {
+        continue
+    }
+    guard let integerInput = Int(givenInput) else {
+        continue
+    }
+    if integerInput < 0 || integerInput > 10 {
+        continue
+    }
+    openedCases = integerInput
+    break
+}
 
 // Now get the actual number of briefcases that were opened during the game
 // e.g.: write the rest of the INPUT section
@@ -18,7 +45,7 @@ var countOfBriefcasesOpened = 3
 // Some output may be given here if you desire
 
 // Collect the list of what briefcases were opened here
-for counter in 1...countOfBriefcasesOpened {
+for counter in 1...openedCases {
     
     // Ask what briefcase was opened in this turn
     print("Briefcase opened, in turn \(counter), was:")
@@ -29,10 +56,27 @@ for counter in 1...countOfBriefcasesOpened {
         // If someone enters nil input, just skip to the next line
         continue
     }
-    
-    // What was provided?
-    print("The given input was: \(givenInput)")
-    
-    // Implement the rest of your logic here...
+    guard let integerInput = Int(givenInput) else {
+        continue
+    }
+    cases.removeValue(forKey: integerInput)
+}
+
+print("What was the banker’s offer?")
+let offer = readLine()
+var average = 0
+for i in 1...cases.count {
+    let temp = cases[i]
+    if temp != nil {
+    average += temp!
+    }
+}
+
+average /= cases.count
+
+if average > Int(offer!)! {
+    print("Deal.")
+} else {
+    print("No deal.")
 }
 
